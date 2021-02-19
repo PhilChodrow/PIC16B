fbf20a85f6d1181a8e2f9c8fc79ff9fd50eddc8c
Phil Chodrow <pchodrow@gmail.com>
2021-02-19 11:06:46 -0800

    add noaa-ghcn data with cleaning

---
+++ datasets/noaa-ghcn/clean-data.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import os 
+# ---------
+# clean and store raw temp data
+# ---------
+
+path = "datasets/noaa-ghcn/ghcnm.tavg.v4.0.1.20210218.qcf.dat"
+
+colnames = ["ID", "Year", "Element"]
+
+colspecs = [(0,11),(11, 15),(15, 19)]
+
+headers = ["VALUE", "DMFLAG", "QCFLAG", "DSFLAG"]
+
+i = 19
+j = 24
+
+for month in range(12):
+    for header in headers:
+        colnames.append(f"{header}{month+1}")
+
+    spacer = 19 + month*8
+    colspecs.append((spacer, spacer + 5))
+    colspecs.append((spacer + 5, spacer + 6))
+    colspecs.append((spacer + 6, spacer + 7))
+    colspecs.append((spacer + 7, spacer + 8))
+
+df = pd.read_fwf(path, 
+                 colspecs = colspecs,
+                 names = colnames)
+
+
+keepnames = [name for name in df.columns if (name in ["ID", "Year"]) or "VALUE" in name]
+df = df[keepnames]
+df = df[df["Year"] > 1900]
+
+import os
+if not os.path.exists('datasets/noaa-ghcn/decades'):
+    os.makedirs('datasets/noaa-ghcn/decades')
+
+for i in range(12):
+    begin = 1901 + i*10
+    end   = begin + 9
+    sub = df[(df["year"] >= begin) && (df["year"] <= end)]
+    # path = f"datasets/noaa-ghcn/decades/test.csv"
+    # sub.to_csv(path)
+
+
+
+# df["country"] = df.ID.str[:2]
+
+df.to_csv("datasets/noaa-ghcn/temps-qcf.csv", index = False)
+
+recent_temps = df[df["Year"] >= 1990]
+df.to_csv("datasets/noaa-ghcn/recent-temps-qcf.csv", index = False)
+
+
+
+
+# ---------
+# clean and store metadata
+# ---------
+
+path = "datasets/noaa-ghcn/ghcnm.tavg.v4.0.1.20210218.qfe.inv"
+colspecs = [(0, 11), (12, 20), (21, 30), (31, 37), (38, 68)]
+colnames = ["ID", "LATITUDE", "LONGITUDE", "STNELEV", "NAME"]
+
+df = pd.read_fwf(path, 
+                 colspecs = colspecs,
+                 names = colnames)
+
+df.to_csv("datasets/noaa-ghcn/station-metadata.csv", )
